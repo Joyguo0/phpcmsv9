9aefdd6b4c58ed21360bc8e4aa8a2aa2b325ea57
joyguo0 <joyguo0@qq.com>
2013-06-18 22:51:36 +0800

    init phpcmsv9

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/phpcmsv9/',
+    'authkey' => '4L1V2ORGHfEk7vNFnvh1uaOavx8B5VbL',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
