a0d1458b45e2059a13a854f530e7e860b3a82bb6
joyguo0 <joyguo0@qq.com>
2013-06-18 23:15:17 +0800

    add yanxiu template and module

--- phpcms/templates/yanxiu/config.php
+++ phpcms/templates/yanxiu/config.php
@@ -1,10 +1,10 @@
 <?php return array (
-  'name' => '默认模板',
-  'author' => 'PHPCMS TEAM',
+  'name' => 'Yanxiu',
+  'author' => 'Joy',
   'dirname' => 'default',
   'homepage' => 'http://www.phpcms.cn/',
   'version' => '1.0',
-  'disable' => 1,
+  'disable' => 0,
   'file_explan' => 
   array (
     'templates|default|content' => 
